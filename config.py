246b69788bf2b613d0d3b007fafb0167d97195ff
niteshsharma1106 <nitesh.sharma1106@gmail.com>
2023-07-26 09:03:59 +0530

    updated

--- config.py
+++ config.py
@@ -3,7 +3,9 @@
 import os
 
 # Set your database URI here
-basepath = os.path.abspath(os.path.pardir)
+basepath = os.path.abspath(os.path.dirname(__file__)) #os.path.abspath(os.path.pardir)
 
-DATABASE_URI = 'sqlite:///'+basepath+'app.db'
+DATABASE_URI = 'sqlite:///' + os.path.join(basepath, 'app.db')
+
+print(DATABASE_URI)
 SECRET_KEY = os.urandom(24)
